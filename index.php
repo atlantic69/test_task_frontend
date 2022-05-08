b0da0a546825ac8e9325f3076d9e31d3b77e5223
Atlantic <polosuhin.99@gmail.com>
2022-05-09 05:54:38 +0700

    Create starting page index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="ru">
+	<head>
+		<meta charset = "utf-8">
+		<meta name = "viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+		integrity = "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin = "anonymous">
+		<title>TTF</title>
+	</head>
+	<header class="p-3 bg-dark">
+	<div class="container">
+		<div class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+			<span class="fs-2"><a href="index.php"><b>Star Wars: </b> Heroes & Vehicles</a></span>
+			<style>
+      			html, body {
+     		    	width: 100%; height: 100%; padding: 0; margin: 0; background: #404044;
+    		    }
+    		    a {
+            		color: #ffe500; /* Цвет ссылки */
+            		text-decoration: none; /* Убираем подчеркивание у ссылок */
+        		}
+        		a:hover {
+        		    color: #ff3333; /* Цвет ссылки при наведении на нее курсора мыши */
+        		}
+    		</style>
+		</div>
+	</div>
+</header>
+
+	<body>
+		
+		<div class = "block">
+			<div class="container p-5 ">
+				<div class="container rounded-3 p-5 bg-warning" style="--bs-bg-opacity: .7;">
+					<h1>Привет</h1>
+				</div>
+			</div>
+		</div>
+		
+
+	</body>
+</html>
